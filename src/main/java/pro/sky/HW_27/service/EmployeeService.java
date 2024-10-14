8d5794699a81680b7ddadfc32eece6ee33dff0e5
Cranky Bawler <6666lenia6666@mail.ru>
2024-10-14 16:32:34 +0300

    1

--- src/main/java/pro/sky/HW_27/service/EmployeeService.java
+++ src/main/java/pro/sky/HW_27/service/EmployeeService.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 
 public interface EmployeeService {
     Employee add(String firstName, String lastName);
+
     Employee remove(String firstName, String lastName);
     Employee find(String firstName, String lastName);
 
--- src/main/java/pro/sky/HW_27/service/EmployeeServiceImp.java
+++ src/main/java/pro/sky/HW_27/service/EmployeeServiceImp.java
@@ -5,43 +5,41 @@ import pro.sky.HW_27.entity.Employee;
 import pro.sky.HW_27.exception.EmployeeAlreadyAddedException;
 import pro.sky.HW_27.exception.EmployeeNotFoundExection;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImp  implements EmployeeService{
     private static final int MAX_SIZE = 100;
-    List<Employee> employees = new ArrayList<>(List.of(
-            new Employee("Бобров", "Ефим"),
-            new Employee("Гуляев", "Тихон"),
-            new Employee("Шашков", "Савели"),
-            new Employee("Николаев", "Остап"),
-            new Employee("Шаров", "Алексей"),
-            new Employee("Вишнякова", "Дарья"),
-            new Employee("Калашникова", "Астра"),
-            new Employee("Беспалова", "Роксана"),
-            new Employee("Лазарева", "Хильда"),
-            new Employee("Уварова", "Дана")
+    Map<String, Employee> employees = new HashMap<>(Map.of(
+            "Ефим Бобров", new Employee("Ефим", "Бобров"),
+            "Тихон Гуляев", new Employee("Тихон", "Гуляев"),
+            "Савели Шашков", new Employee("Савели", "Шашков"),
+            "Остап Николаев", new Employee("Остап", "Николаев"),
+            "Алексей Шаров", new Employee("Алексей", "Шаров"),
+            "Дарья Вишнякова", new Employee("Дарья", "Вишнякова"),
+            "Астра Калашникова", new Employee("Астра", "Калашникова"),
+            "Роксана Беспалова", new Employee("Роксана", "Беспалова"),
+            "Хильда Лазарева", new Employee("Хильда", "Лазарева"),
+            "Дана Уварова", new Employee("Дана", "Уварова")
     ));
 
     @Override
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
+
+
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
-            employees.remove(employee);
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+           return employees.remove(employee.getFullName());
         }
         throw new EmployeeNotFoundExection();
     }
@@ -49,15 +47,15 @@ public class EmployeeServiceImp  implements EmployeeService{
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+            return employees.get(employee.getFullName());
         }
         throw new EmployeeNotFoundExection();
     }
 
     @Override
     public Collection<Employee> findAll() {
-        return Collections.unmodifiableList(employees);
+        return Collections.unmodifiableCollection(employees.values());
     }
 
 }
